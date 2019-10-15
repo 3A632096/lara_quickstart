0487918936a9dde68a2809a4098e4d07c86ba615
Yu Qin <zxas5820@yahoo.com.tw>
2019-10-15 22:32:59 +0800

    初步建置三個路由

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+// 增加新的任務
+Route::post('/task', function (Request $request) {
+    //
+});
+// 刪除任務
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
